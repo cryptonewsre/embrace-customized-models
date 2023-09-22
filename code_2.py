38dc3ff367202f8949a0dcaf2343378318c07935
cryptonewsre <139812145+cryptonewsre@users.noreply.github.com>
2023-09-22 02:50:13 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,12 @@
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+}
+        }
+        for (int item : data) {
+    }
+        }
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+
+    public static void main(String[] args) {
+public class RandomDataGenerator {
